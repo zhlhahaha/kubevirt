2c4ca6d8c1c7ebc2dad9a7852880f7a36ff27334
Howard Zhang <howard.zhang@arm.com>
2021-01-13 07:36:30 +0000

    test for rpm-build for aarch64
    Signed-off-by: Howard Zhang <howard.zhang@arm.com>

--- hack/rpm-deps.sh
+++ hack/rpm-deps.sh
@@ -18,74 +18,19 @@ bazel run \
     --config=${ARCHITECTURE} \
     //:bazeldnf -- fetch
 
-# create a rpmtree for our test image with misc. tools.
-bazel run \
-    --config=${ARCHITECTURE} \
-    //:bazeldnf -- rpmtree --public --name testimage_x86_64 \
-    $basesystem \
-    qemu-img \
-    qemu-guest-agent \
-    stress \
-    dmidecode \
-    virt-what \
-    which \
-    nginx \
-    scsi-target-utils \
-    procps-ng \
-    nmap-ncat \
-    iputils \
-    e2fsprogs
-
-bazel run \
-    --config=${ARCHITECTURE} \
-    //:bazeldnf -- rpmtree --public --arch=ppc64le --name testimage_ppc64le \
-    $basesystem \
-    qemu-img \
-    qemu-guest-agent \
-    stress \
-    nginx \
-    scsi-target-utils \
-    procps-ng \
-    nmap-ncat \
-    iputils \
-    e2fsprogs
-
-# create a rpmtree for libvirt-devel. libvirt-devel is needed for compilation and unit-testing.
-bazel run \
-    --config=${ARCHITECTURE} \
-    //:bazeldnf -- rpmtree --public --name libvirt-devel_x86_64 $basesystem libvirt-devel-${LIBVIRT_VERSION}
-
 # create a rpmtree for virt-launcher and virt-handler. This is the OS for our node-components.
 bazel run \
     --config=${ARCHITECTURE} \
-    //:bazeldnf -- rpmtree --public --name launcherbase_x86_64 \
-    $basesystem \
-    libverto-libev \
-    libvirt-daemon-driver-qemu-${LIBVIRT_VERSION} \
-    libvirt-client-${LIBVIRT_VERSION} \
-    libvirt-daemon-driver-storage-core-${LIBVIRT_VERSION} \
-    qemu-kvm-${QEMU_VERSION} \
-    seabios-${SEABIOS_VERSION} \
-    genisoimage \
-    selinux-policy selinux-policy-targeted \
-    nftables \
-    findutils \
-    procps-ng \
-    iptables
+    //:bazeldnf -- rpmtree --public --arch=aarch64 --name libvirt-devel_aarch64 $basesystem libvirt-devel-${LIBVIRT_VERSION}
 
-# remove all RPMs which are no longer referenced by a rpmtree
 bazel run \
     --config=${ARCHITECTURE} \
     //:bazeldnf -- prune
 
-# FIXME: For an unknown reason the run target afterwards can get
-# out dated tar files, build them explicitly first.
 bazel build \
     --config=${ARCHITECTURE} \
-    //rpm:libvirt-devel_x86_64
+    //rpm:libvirt-devel_aarch64
 
-# update tar2files targets which act as an adapter between rpms
-# and cc_library which we need for virt-launcher and virt-handler
 bazel run \
     --config=${ARCHITECTURE} \
-    //rpm:ldd
+    //rpm:ldd_aarch64
